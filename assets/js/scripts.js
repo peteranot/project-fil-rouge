d440587b61e266cbe4be45b40a41584324d73e77
peteranot <terehau.peterano@gmail.com>
2021-04-22 14:02:22 -1100

    mise a jour du menu (bouton: theme)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,17 +24,23 @@ themeChangerBtnElem.addEventListener('click',()=>{
   updateUI();
 });
 
+
 /* FOONCTION updateUI() --> Attribue la couleurs et le texte du thème" */
 updateUI();
 function updateUI(){
     if(localStorage.currentTheme == "color1"){
-      document.body.style.setProperty("--themeColor", "white")
-      document.body.style.setProperty("--themeTextColor", "black")
-      document.getElementById("changerNom").innerHTML = "Claire"; /* Changer le texte du thème en "Claire" */
+      document.body.style.setProperty("--themeColor", 'white')
+      document.body.style.setProperty("--themeTextColor", 'black')
+      document.getElementById('btnTheme').style.backgroundColor='white' /* Changer la couleur du bg en blanc */
+      document.getElementById('btnTheme').style.color='black' /* Changer la couleur du texte en noir */
+      document.getElementById("changerNom").innerHTML = 'Claire' /* Changer le texte du thème en "Claire" */
     }else if (localStorage.currentTheme == "color2"){
       document.body.style.setProperty("--themeColor", "black")
-      document.getElementById("changerNom").innerHTML = "Sombre"; /* Changer le texte du thème en "Sombre" */
-      document.body.style.setProperty("--themeTextColor", "white")
+      document.getElementById("changerNom").innerHTML = 'Sombre'; /* Changer le texte du thème en "Sombre" */
+      document.body.style.setProperty("--themeTextColor", 'white')
+      document.body.style.setProperty("--bgbtnthemeColor", 'white')
+      document.getElementById('btnTheme').style.backgroundColor='black' /* Changer la couleur du bg en noir */
+      document.getElementById('btnTheme').style.color='#fff'; /* Changer la couleur du texte en blanc */
     }
 
   };
